760938219b75302168021291e3cf4fc4a227ae70
Mohamed Hossam <https://github.com/mohamedhossamkaml>
2025-11-06 22:50:56 +0200

    fix: Galary

--- .history/src/config/galleryImagesDetails_20251106224836.ts
+++ .history/src/config/galleryImagesDetails_20251106224836.ts
@@ -3,17 +3,17 @@
 
 const GALLERY_IMAGES_DETAILS: Record<number, string[]> = {
   0: [
-    'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?auto=format&fit=crop&w=1800&q=80',
-    'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1800&q=80',
-    'https://cdn.pixabay.com/photo/2017/03/26/16/06/exzenterhaus-2175925_1280.jpg',
+  'https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?auto=format&fit=crop&w=1800&q=80',
+  'https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1800&q=80',
+  'https://cdn.pixabay.com/photo/2017/03/26/16/06/exzenterhaus-2175925_1280.jpg',
   ],
   1: [
-    'https://cdn.pixabay.com/photo/2024/03/07/15/57/houses-8618837_1280.jpg',
-    'https://cdn.pixabay.com/photo/2017/04/10/22/28/residence-2219972_1280.jpg',
+  'https://cdn.pixabay.com/photo/2024/03/07/15/57/houses-8618837_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/04/10/22/28/residence-2219972_1280.jpg',
   ],
   2: [
-    'https://cdn.pixabay.com/photo/2019/10/26/11/40/building-4579244_1280.jpg',
-    'https://cdn.pixabay.com/photo/2017/07/08/06/48/sa-rapita-2483668_1280.jpg',
+  'https://cdn.pixabay.com/photo/2019/10/26/11/40/building-4579244_1280.jpg',
+  'https://cdn.pixabay.com/photo/2017/07/08/06/48/sa-rapita-2483668_1280.jpg',
   ],
   3: [
     'https://cdn.pixabay.com/photo/2017/04/10/22/28/residence-2219972_1280.jpg',
@@ -28,15 +28,14 @@ const GALLERY_IMAGES_DETAILS: Record<number, string[]> = {
     'https://cdn.pixabay.com/photo/2015/12/15/08/53/city-1093864_1280.jpg',
   ],
   6: [
-    'https://cdn.pixabay.com/photo/2017/07/08/02/16/house-2483336_1280.jpg',
     'https://cdn.pixabay.com/photo/2019/10/27/08/17/villa-4581027_1280.jpg',
+    'https://cdn.pixabay.com/photo/2015/12/15/08/53/city-1093864_1280.jpg',
   ],
   7: [
     'https://cdn.pixabay.com/photo/2019/10/27/08/17/villa-4581027_1280.jpg',
     'https://cdn.pixabay.com/photo/2015/12/15/08/53/city-1093864_1280.jpg',
   ],
   8: [
-    'https://cdn.pixabay.com/photo/2017/07/08/02/16/house-2483336_1280.jpg',
     'https://cdn.pixabay.com/photo/2019/10/27/08/17/villa-4581027_1280.jpg',
     'https://cdn.pixabay.com/photo/2015/12/15/08/53/city-1093864_1280.jpg',
   ],
